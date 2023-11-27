60c25a79d57c3ef9dfa8cf0f66ae28925bd9fa3a
Pooja Ganorkar <pooja.ganorkar@grow.inc>
2023-11-27 15:52:21 +0530

    modified test steps

--- src/dlta_online/pom/dashboard_page.ts
+++ src/dlta_online/pom/dashboard_page.ts
@@ -3,87 +3,86 @@ import { BasePage } from "../../common/pom/base_page";
 import { AddCase } from "./component/addcase";
 import * as caseManagement from "../data/closed_case_data.json";
 
-export class DashboardPage extends BasePage { 
+export class DashboardPage extends BasePage {
 
-    readonly addCaseLink: Locator;
-    readonly addCase: AddCase;
-    readonly case_management: Locator;
-    readonly closed_cases: Locator;
-    readonly filter: Locator;
-    readonly go_option: Locator;
-     readonly memberAccNumber: Locator;
-     readonly apply:Locator;
-     readonly text_Box: Locator;
-     readonly items:Locator;
-	   readonly caseGroupId:Locator;
-     readonly memberText:Locator;
-     readonly filter_dropdown: Locator;
-     readonly close_left: Locator;
+  readonly addCaseLink: Locator;
+  readonly addCase: AddCase;
+  readonly case_management: Locator;
+  readonly closed_cases: Locator;
+  readonly filter: Locator;
+  readonly go_option: Locator;
+  readonly memberAccNumber: Locator;
+  readonly apply: Locator;
+  readonly text_Box: Locator;
+  readonly items: Locator;
+  readonly caseGroupId: Locator;
+  readonly memberText: Locator;
+  readonly filter_dropdown: Locator;
+  readonly close_left: Locator;
 
-    constructor(page: Page) {
-        super(page)
+  constructor(page: Page) {
+    super(page)
 
-        this.addCaseLink = page.getByRole('button', { name: 'add-circle icon Add New Case' });
-        this.addCase = new AddCase(page);
-        this.case_management= page.getByRole('heading', { name: 'Case Management' });
-        this.closed_cases= page.getByRole('link', { name: 'Closed Cases' });
-        this.filter= page.getByRole('button', { name: 'FILTER' });
-        this.go_option= page.getByRole('button', { name: 'Go' });
-        this.memberAccNumber = page.getByText('Member Account Number');
-        this.apply = page.getByRole('button', { name: 'APPLY' });
-        this.text_Box =  page.getByRole('textbox');
-        this.items = page.locator('//div[contains(@class,"filter-list-item")]');
-        this.memberText = page.locator('span');
-        this.caseGroupId = page.getByText('Case Group ID');
-        this.filter_dropdown = page.locator('li').filter({ hasText: /^Admin User$/ });
-        this.close_left =  page.getByRole('button', { name: 'arrow-left icon clipboard-tick icon' });
+    this.addCaseLink = page.getByRole('button', { name: 'add-circle icon Add New Case' });
+    this.addCase = new AddCase(page);
+    this.case_management = page.getByRole('heading', { name: 'Case Management' });
+    this.closed_cases = page.getByRole('link', { name: 'Closed Cases' });
+    this.filter = page.getByRole('button', { name: 'FILTER' });
+    this.go_option = page.getByRole('button', { name: 'Go' });
+    this.memberAccNumber = page.getByText('Member Account Number');
+    this.apply = page.getByRole('button', { name: 'APPLY' });
+    this.text_Box = page.getByRole('textbox');
+    this.items = page.locator('//div[contains(@class,"filter-list-item")]');
+    this.memberText = page.locator('span');
+    this.caseGroupId = page.getByText('Case Group ID');
+    this.filter_dropdown = page.locator('li').filter({ hasText: /^Admin User$/ });
+    this.close_left = page.getByRole('button', { name: 'arrow-left icon clipboard-tick icon' });
+  }
+
+  async addNewCase() {
+    await this.addCaseLink.click();
+    await this.addCase.submitCase();
+  }
+
+  async maximizeWindow() {
+    await this.page.setViewportSize({ width: 1920, height: 1080 });
+  }
+
+  async waitForTimeout(milliseconds: number) {
+    await this.page.waitForTimeout(milliseconds); // Wait for the specified duration in milliseconds
+  }
+
+  async verifyCaseManagementButtons() {
+    await this.closed_cases.isVisible();
+    await this.closed_cases.click();
+    await this.filter.isVisible();
+    await this.go_option.isVisible();
+  }
+
+  async clickOnFilter() {
+    await this.filter.click();
+  }
+
+  async getListOfItems() {
+    let items: string[] = [];
+    let els = await this.items.all();
+    for (const e of els) {
+      const text = await e.textContent();
+      items.push(text || "");
+      console.log("Element Text:", text);
     }
+    console.log("Generated items:", items);
+    return items
+  }
 
-    async addNewCase(){
-        await this.addCaseLink.click();  
-        await this.addCase.submitCase();     
-    }
+  async verifyMemberAccNumber() {
+    await this.memberAccNumber.click();
+    await this.text_Box.fill(caseManagement.accountNumber);
+    await this.apply.click();
+    await this.go_option.click();
+  }
 
-    async maximizeWindow() {
-        await this.page.setViewportSize({ width: 1920, height: 1080 });
-      }
-  
-      async waitForTimeout(milliseconds: number) {
-        await this.page.waitForTimeout(milliseconds); // Wait for the specified duration in milliseconds
-      }
-
-    async verifyCaseManagementButtons(){
-        await this.closed_cases.isVisible();
-        await this.closed_cases.click();
-        await this.filter.isVisible();
-        await this.go_option.isVisible();
-    }
-
-    async clickOnFilter(){
-        await this.filter.click();
-    }
-
-    async getListOfItems() {
-        let items: string[] = [];
-        let els = await this.items.all();
-        for (const e of els) {
-          const text = await e.textContent();
-          items.push(text || "");
-          console.log("Element Text:", text);
-        }
-        console.log("Generated items:", items);
-        return items
-    }
-  
-      async verifyMemberAccNumber() {
-        await this.memberAccNumber.click();
-        await this.text_Box.fill(caseManagement.accountNumber);
-        await this.apply.click();
-        await this.go_option.click(); 
-      }
-
-      async clickOnClosedIcon(){
-        await this.close_left.click();
-    
-      }
+  async clickOnClosedIcon() {
+    await this.close_left.click();
+  }
 }
--- tests/dlta_online/dlta_case_mgmnt_close_case.spec.ts
+++ tests/dlta_online/dlta_case_mgmnt_close_case.spec.ts
@@ -5,41 +5,33 @@ import { Admins } from "../../src/dlta_online/data/admins";
 test.describe("Test describe block @casemanagement", () => {
     test.setTimeout(60000);
 
-test.beforeEach(async ({ loginPage, dashboardPage }) => {
-    let admin = Admins.getAdminByUsername("admin@tinasuper.com");
-    await loginPage.navigateTo();
-    await loginPage.doLogin(admin.username, admin.password);
-    await dashboardPage.maximizeWindow();
-});
-
-
-        test("Ensure dashboard is correctly displayed", async ({ dashboardPage }) => {
-            await dashboardPage.closed_cases.click();
-            await dashboardPage.waitForTimeout(3000);
-            await expect(dashboardPage.case_management).toHaveText("Case Management");      
-
-        });
-
-        test("Ensure cases are correctly displayed under Closed Cases tab", async ({ dashboardPage }) => {
-            await dashboardPage.closed_cases.click(); 
-            await dashboardPage.verifyCaseManagementButtons();
-            
-        });
-
-        test("Ensure the user can successfully filter on multiple parameters in Case Management Closed Cases", async ({ dashboardPage }) => {
-            await dashboardPage.waitForTimeout(3000);
-            await dashboardPage.closed_cases.click();
-            await dashboardPage.clickOnFilter();
-           let expectedItems = ["Member Account Number", "Effective Date", "Assigned to","Case Type","Outcome","Case Group ID","Status","Reference"];  
-            let actualItems = await dashboardPage.getListOfItems();
-            expect(actualItems).toEqual(expectedItems); 
-         });
-
-         test("Ensure that a member is able to find his case based on his account number", async ({ dashboardPage }) => {
-            //await dashboardPage.clickOnClosedIcon();
-            await dashboardPage.clickOnFilter();
-            await dashboardPage.verifyMemberAccNumber();
-            expect(dashboardPage.memberText.filter({ hasText: ('Member Account Number: 4F653-90-') })); 
-        });
-        
+    test.beforeEach(async ({ loginPage, dashboardPage }) => {
+        let admin = Admins.getAdminByUsername("admin@tinasuper.com");
+        await loginPage.navigateTo();
+        await loginPage.doLogin(admin.username, admin.password);
+        await dashboardPage.maximizeWindow();
     });
+
+    test("Ensure dashboard is correctly displayed", async ({ dashboardPage }) => {
+        await dashboardPage.waitForTimeout(3000);
+        await expect(dashboardPage.case_management).toHaveText("Case Management");
+    });
+
+    test("Ensure cases are correctly displayed under Closed Cases tab", async ({ dashboardPage }) => {
+        await dashboardPage.closed_cases.click();
+        await dashboardPage.verifyCaseManagementButtons();
+    });
+
+    test("Ensure the user can successfully filter on multiple parameters in Case Management Closed Cases", async ({ dashboardPage }) => {
+        await dashboardPage.waitForTimeout(3000);
+        await dashboardPage.closed_cases.click();
+        await dashboardPage.clickOnFilter();
+        let expectedItems = ["Member Account Number", "Effective Date", "Assigned to", "Case Type", "Outcome", "Case Group ID", "Status", "Reference"];
+        let actualItems = await dashboardPage.getListOfItems();
+        expect(actualItems).toEqual(expectedItems);
+        await dashboardPage.verifyMemberAccNumber();
+        expect(dashboardPage.memberText.filter({ hasText: ('Member Account Number: 4F653-90-') }));
+        await dashboardPage.go_option.click();
+    });
+
+});
