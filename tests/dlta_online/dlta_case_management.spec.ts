7c96b7ca5605314d8e8d682d57882c9cb29a36d6
pharish <pharish.kumar@grow.inc>
2023-11-27 13:33:21 +0530

    Added validation check for alerts

--- tests/dlta_online/dlta_case_management.spec.ts
+++ tests/dlta_online/dlta_case_management.spec.ts
@@ -1,76 +1,63 @@
-import { dltaOnlineTest as test } from "../../src/dlta_online/base_dlta_online_test"
-import { Admins } from "../../src/dlta_online/data/admins"
+import { dltaOnlineTest as test } from "../../src/dlta_online/base_dlta_online_test";
+import { Admins } from "../../src/dlta_online/data/admins";
 import { expect } from "@playwright/test";
+
 test.describe("Test describe block @casemanagement", () => {
-    
     test.setTimeout(60000);
-    test("Casemanagement DetailsPage", async({loginPage,dashboardPage}) => {
+
+    test.beforeEach(async ({ loginPage, dashboardPage }) => {
         let admin = Admins.getAdminByUsername("admin@tinasuper.com");
+        await loginPage.navigateTo();
+        await loginPage.doLogin(admin.username, admin.password);
+        await dashboardPage.maximizeWindow();
+    });
 
-        await test.step("Login", async () => {
-            await loginPage.navigateTo();
-            await loginPage.doLogin(admin.username, admin.password);
-            await dashboardPage.maximizeWindow();
-        })
+    test("Ensure cases are correctly displayed under Open Cases tab", async ({ dashboardPage }) => {
+        await dashboardPage.waitForTimeout(3000);
+        await expect(dashboardPage.casemanagement).toHaveText("Case Management");
+        await dashboardPage.takeScreenshot('HomePage.png');
+    });
 
-        await test.step("To validate casemanagement dashboard screen", async () => {
-            
-            await dashboardPage.waitForTimeout(3000)
-            await expect(dashboardPage.casemanagement).toHaveText("Case Management"); 
-            await dashboardPage.takeScreenshot('HomePage.png');
-            
-        })
-    
-        await test.step("To verify whether member able to see the tabs on Case Management screen", async () => {
-            await dashboardPage.verifyCaseManagementTabs();
-            await dashboardPage.takeScreenshot('Tabs.png');
-        })
-        await test.step("To verify filtering is available on Open Cases in Case Management", async () => {
-            let expectedItems = ["Member Account Number", "Effective Date", "Assigned to","Case Type","Case Group ID","Reference","Status","Outcome"]
-            let actualItems = await dashboardPage.getListItems();
-            expect(actualItems).toEqual(expectedItems) 
-            await dashboardPage.takeScreenshot('Filter.png');
-        })
-		await test.step("To verify that an existing case can be assigned to a user", async() => {
-            const rowNumberToClick = 2;
-            await dashboardPage.clickOnTableRow(rowNumberToClick);
-            await dashboardPage.addCaseToAssignee();
-            await dashboardPage.sleep(5000);
-            await expect(dashboardPage.activity_text).toContainText("Case Assigned to"); 
-            await dashboardPage.takeScreenshot('assignee.png');
-            
-        })
-        await test.step("To verify that an existing case can be assigned to a member FilterOption:AssignedTo", async () => {            
-			await dashboardPage.clickOnClosedIcon();  
-            await dashboardPage.clickFilter();
-            await dashboardPage.verify_Member_TobeAssigned();
-            await dashboardPage.apply_button();
-            await dashboardPage.go_Button();
-            await dashboardPage.waitForTimeout(5000);
-            const rowNumberToClick = 1;
-            await dashboardPage.clickOnTableRow(rowNumberToClick);
-            await dashboardPage.addCaseToAssignee();
-            await dashboardPage.sleep(5000);
-            await dashboardPage.takeScreenshot('assignedTo.png');
-            await expect(dashboardPage.activity_text).toContainText("Case Assigned to"); 
-        })
+    test("Ensure there are following tabs on Case Management screen: Open Cases, Closed Cases, On Hold, SLA", async ({ dashboardPage }) => {
+        await dashboardPage.verifyCaseManagementTabs();
+        await dashboardPage.waitForTimeout(50000)
+        await dashboardPage.takeScreenshot('Tabs.png');
+    });
 
-        // await test.step("To verify that an existing case can be assigned to a member FilterOption:caseGroupId", async () => {            
-        //     await dashboardPage.clickOnClosedIcon();
-        //     await dashboardPage.clickFilter();
-        //     await dashboardPage.basedOnCaseId();
-        //     await dashboardPage.apply_button();
-        //     await dashboardPage.go_Button();
-        //     const rowNumberToClick = 1;
-        //     await dashboardPage.clickOnTableRow(rowNumberToClick);
-        //     await dashboardPage.addCaseToAssignee();
-        //     await new Promise(() => {})
-            
-        // })
-		
-    })
+    test("Ensure the user can successfully filter on multiple parameters in Case Management Open Cases", async ({ dashboardPage }) => {
+        await dashboardPage.clickFilter();
+        await dashboardPage.waitForTimeout(50000);
+        let expectedItems = ["Member Account Number", "Effective Date", "Assigned to", "Case Type", "Case Group ID", "Reference", "Status", "Outcome"];
+        let actualItems = await dashboardPage.getListItems();
+        expect(actualItems).toEqual(expectedItems);
+        await dashboardPage.takeScreenshot('Filter.png');
+    });
 
+    test("Ensure that an existing case can be assigned to a user", async ({ dashboardPage }) => {
+        await dashboardPage.sleep(5000);
+        //await dashboardPage.waitForTimeout(50000);
+        const rowNumberToClick = 2;
+        await dashboardPage.clickOnTableRow(rowNumberToClick);
+        await dashboardPage.addCaseToAssignee();
+        await dashboardPage.sleep(5000);
+        await expect(dashboardPage.activity_text).toContainText("Case Assigned to");
+        await dashboardPage.takeScreenshot('assignee.png');
+    });
 
-
-})
-  
+    test("FilterOption:AssignedTo", async ({ dashboardPage }) => {
+        await dashboardPage.clickFilter();
+        await dashboardPage.verify_Member_TobeAssigned();
+        await dashboardPage.apply_button();
+        await dashboardPage.go_Button();
+        await dashboardPage.waitForTimeout(5000);
+        const expectedAlertText = 'Assigned to: Admin User';
+        const actualAlertText = await dashboardPage.alert_displayed();
+        if (actualAlertText !== null) {
+            expect(actualAlertText).toContain(expectedAlertText);
+          } else {
+            console.error('No alert was displayed.');
+          }
+        await dashboardPage.takeScreenshot('assignedTo.png');
+        
+    });
+});
