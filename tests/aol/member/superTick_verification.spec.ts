64805868fa5862ffe85e82ef93687c6697a95e72
anilkumar.shanthalingappa <anilkumar.shanthalingappa@grow.inc>
2024-03-13 18:53:16 +0530

    Supertick test update

--- tests/aol/member/superTick_verification.spec.ts
+++ tests/aol/member/superTick_verification.spec.ts
@@ -2,6 +2,7 @@ import { aolTest as test } from "../../../src/aol/base_aol_test"
 import { allure } from "allure-playwright";
 import { fundName } from "../../../src/aol/utils_aol";
 import { AccumulationMemberApiHandler } from "../../../src/aol_api/handler/member_creation_accum_handler";
+import * as member from "../../../src/aol/data/member.json"
 
 test.beforeEach(async ({ navBar }) => {
     test.setTimeout(600000);
@@ -13,17 +14,19 @@ test.beforeEach(async ({ navBar }) => {
 test(fundName()+"Verify Super Tick status is Matched for an Active Super member when a valid TFN is updated for the member @superTick", async ({ navBar, memberOverviewpage, relatedInformationPage ,memberApi,accountInfoPage}) => {
 
     try {
-        await navBar.navigateToAccumulationMembersPage();
-        let { memberNo ,processId} = await AccumulationMemberApiHandler.createMember(memberApi);
-        await accountInfoPage.ProcessTab();
-        const caseGroupId = await AccumulationMemberApiHandler.getCaseGroupId(memberApi,processId);
-        await AccumulationMemberApiHandler.approveProcess(memberApi,caseGroupId!);
-        await new Promise(resolve => setTimeout(resolve, 10000));
-        await accountInfoPage.reload();
+        
+        // let { memberNo ,processId} = await AccumulationMemberApiHandler.createMember(memberApi);
+        // await new Promise(resolve => setTimeout(resolve, 5000));
+        // const caseGroupId = await AccumulationMemberApiHandler.getCaseGroupId(memberApi,processId);
+        // await new Promise(resolve => setTimeout(resolve, 5000));
+        // await AccumulationMemberApiHandler.approveProcess(memberApi,caseGroupId!);
+
+        let memberNo = member.memberID;
         await navBar.navigateToAccumulationMembersPage();
         await navBar.selectMember(memberNo);
         await memberOverviewpage.superTickVerification();
         await navBar.navigateToAccumulationMembersPage();
+        await navBar.selectMember(memberNo);
         await relatedInformationPage.verifySuperTickStatus(true);
         
     } catch (error) {
@@ -31,20 +34,21 @@ test(fundName()+"Verify Super Tick status is Matched for an Active Super member 
     }
 })
 
-test(fundName()+"Verify that for a member with 'Provisional' status No super tick call is made. @superTick", async ({ navBar, memberOverviewpage, relatedInformationPage,accountInfoPage,memberApi }) => {
+test(fundName()+"Verify that for a member with 'Provisional' status No super tick call is made. @superTick", async ({ navBar, memberOverviewpage, relatedInformationPage, memberApi }) => {
 
     try {
-        await navBar.navigateToAccumulationMembersPage();
+
         let { memberNo ,processId} = await AccumulationMemberApiHandler.createMember(memberApi);
-        await accountInfoPage.ProcessTab();
+        await new Promise(resolve => setTimeout(resolve, 5000));
         const caseGroupId = await AccumulationMemberApiHandler.getCaseGroupId(memberApi,processId);
+        await new Promise(resolve => setTimeout(resolve, 5000));
         await AccumulationMemberApiHandler.approveProcess(memberApi,caseGroupId!);
-        await new Promise(resolve => setTimeout(resolve, 10000));
-        await accountInfoPage.reload();
+
         await navBar.navigateToAccumulationMembersPage();
         await navBar.selectMember(memberNo);
         await memberOverviewpage.superTickVerification();
         await navBar.navigateToAccumulationMembersPage();
+        await navBar.selectMember(memberNo);
         await relatedInformationPage.verifySuperTickStatus(false);
         
     } catch (error) {
