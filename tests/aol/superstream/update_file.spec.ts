7c8b6deefe6e9f62e8247ddd5aaae7cf476a225e
minal.tate <minal.tate@U-2LFJPIJ34CL87.grow.inc>
2024-04-05 12:58:06 +0530

    feat:update xml code

--- tests/aol/superstream/update_file.spec.ts
+++ tests/aol/superstream/update_file.spec.ts
@@ -12,7 +12,7 @@ import { DataUtils } from '../../../src/utils/data_utils';
 export function updateFile(): void {
     const templateFileName: string = "xmlFileTemplate.xml";
     console.log(templateFileName)
- 
+
     //const templateFilePath = path.join(__dirname, `../../src/aol/data/${templateFileName}`);
     const absolutePath = path.join(DataUtils.dataFilesPath);
     console.log(absolutePath)
@@ -20,18 +20,18 @@ export function updateFile(): void {
     console.log(destifolder)
 
     let formattedDate: string = getCurrentDateFormatted();
-    const conversationId: string = `Contribution.84111122223.${formattedDate}111812600`;
+    const conversationId: string = `Contribution.84111122223.${formattedDate}111812604`;
     const superGateMessageId: string = `${formattedDate}.115734.123@superchoice.com.au`
 
     const fileName: string = `MRR_${formattedDate}_115734_123_${conversationId}.XML`;
     const fullFileName: string = copyGeneratedFile(absolutePath, templateFileName, fileName, destifolder);
     updateXML(`${destifolder}/${fileName}`, superGateMessageId, conversationId);
 
-   // if (fullFileName) {
+    // if (fullFileName) {
     //  console.error("Failed to copy the template file to template File path folder");
     //} else {
     //    updateXML(`${destifolder}\\${fileName}`, superGateMessageId, conversationId);
-   // }
+    // }
 }
 
 function getCurrentDateFormatted(): string {
@@ -47,14 +47,14 @@ export function copyGeneratedFile(absolutePath: string, templateFileName: string
 
         console.log(files)
         for (const file of files) {
-          //  if (file.includes(templateFileName)) {
-                const fullFilePath = path.join(absolutePath, file);
-                console.log(fullFilePath)
-                const destinationPath = path.join(destifolder, fileName);
-                console.log(destinationPath)
-                fs.copyFileSync(fullFilePath, destinationPath);
-               // console.log(`File ${file} copied successfully to ${destinationPath}`);
-                return file;
+            //  if (file.includes(templateFileName)) {
+            const fullFilePath = path.join(absolutePath, file);
+            console.log(fullFilePath)
+            const destinationPath = path.join(destifolder, fileName);
+            console.log(destinationPath)
+            fs.copyFileSync(fullFilePath, destinationPath);
+            // console.log(`File ${file} copied successfully to ${destinationPath}`);
+            return file;
             //}
         }
 
@@ -67,8 +67,6 @@ export function copyGeneratedFile(absolutePath: string, templateFileName: string
 }
 
 
-
-
 ///update node value
 
 export function updateXML(filePath: string, conversationId: string, superGateMessageId: string): void {
@@ -78,8 +76,8 @@ export function updateXML(filePath: string, conversationId: string, superGateMes
     const conversationId_nodePath: string = "//*[text()='conversationId']";
     const tfn_nodePath: string = "//*[text()='tfn']";
 
-    setListOfXmlValue(filePath, messageId_nodePath, conversationId);
-    setListOfXmlValue(filePath, conversationId_nodePath, superGateMessageId);
+    setListOfXmlValue(filePath, messageId_nodePath, superGateMessageId);
+    setListOfXmlValue(filePath, conversationId_nodePath, conversationId);
     setListOfXmlValue(filePath, tfn_nodePath, "57885");
 
     console.log("XML file updated successfully");
@@ -87,19 +85,49 @@ export function updateXML(filePath: string, conversationId: string, superGateMes
 
 function setListOfXmlValue(filePath: string, nodePath: string, value: string): void {
     try {
-        const xmlContent = fs.readFileSync(filePath, 'utf-8');
-        const xmlDoc = new DOMParser().parseFromString(xmlContent);
+        // const xmlContent = fs.readFileSync(filePath, 'utf-8');
+        // const xmlDoc = new DOMParser().parseFromString(xmlContent);
 
-        const nodes = xpath.select(nodePath, xmlDoc);
+        // // const nodes = xpath.select(nodePath, xmlDoc)?.toString();
+        // // console.log(nodes);
+        // // for (let node of nodes!) {
+        // //     node = value;
+        // // }
+        // const nodesToUpdate = xmlDoc.getElementsByTagName(`superGateMessageId`);
+        // for (let i = 0; i < nodesToUpdate.length; i++) {
+        //     // Modify the node as required
+        //     nodesToUpdate[i].textContent = `testSGM`;
+        // }
+        // const updatedXmlContent = new XMLSerializer().serializeToString(xmlDoc);
+        // require('fs').writeFileSync(filePath, updatedXmlContent);
+        // const builder = new xml2js.Builder();
+        // const updatedXmlContent = builder.buildObject(xmlDoc);
 
-        for (const node of nodes!) {
-            node.textContent = value;
+        // fs.writeFileSync(filePath, updatedXmlContent, 'utf-8');
+
+       // Read the XML file
+        const xmlContent = require('fs').readFileSync(filePath, 'utf-8');
+
+        // Parse XML content
+        const parser = new DOMParser();
+        const xmlDoc = parser.parseFromString(xmlContent, 'text/xml');
+
+        // Update nodes
+        const nodesToUpdate = xmlDoc.getElementsByTagName('genre');
+        for (let i = 0; i < nodesToUpdate.length; i++) {
+            // Modify the node text content as required
+            nodesToUpdate[i].textContent = "geography";
         }
 
+        // Serialize the updated XML content
+        // let updatedXmlContent = new XMLSerializer();
+        // updatedXmlContent.serializeToString(xmlDoc);
         const builder = new xml2js.Builder();
         const updatedXmlContent = builder.buildObject(xmlDoc);
 
-        fs.writeFileSync(filePath, updatedXmlContent, 'utf-8');
+
+        // Write the updated XML content back to the file
+        require('fs').writeFileSync(filePath, updatedXmlContent);
         console.log(`Values updated successfully for nodes at path: ${nodePath}`);
     } catch (error) {
         console.error(`An error occurred while updating XML: ${error}`);
