07823be3e4497b3c4369bd3c95b1719a81b79d0b
anilkumar.shanthalingappa <anilkumar.shanthalingappa@grow.inc>
2024-04-08 20:40:43 +0530

    Superstream mrr with and without TFN tests

--- src/utils/data_utils.ts
+++ src/utils/data_utils.ts
@@ -16,5 +16,4 @@ export class DataUtils {
         return content;
 
     }
-__
 }
--- tests/aol/superstream/file_create.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from '@playwright/test';
-import { updateFile, copyGeneratedFile, updateXML } from '../superstream/update_file.spec'
-
-
-test("updating xml file", async () => {
-    updateFile();
-
-})
