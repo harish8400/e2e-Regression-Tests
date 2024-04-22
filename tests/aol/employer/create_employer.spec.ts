dc0e88ab1dc5551bd4f4782dd4f0275a2096c061
anilkumar.shanthalingappa <anilkumar.shanthalingappa@grow.inc>
2024-04-22 21:12:55 +0530

    Test updates for buildkite

--- tests/aol/employer/create_employer.spec.ts
+++ tests/aol/employer/create_employer.spec.ts
@@ -41,21 +41,21 @@ test("Verify if new employer ABN is created successfully @demorun", async ({ emp
 
 })
 
-// test("Verify if new employer WPN is created successfully @demorun", async ({ employerIdentitiesPage }) => {
+test("Verify if new employer WPN is created successfully @demorun", async ({ employerIdentitiesPage }) => {
 
-//     await employerIdentitiesPage.createNewEmployerWPN('Businness13s');
+    await employerIdentitiesPage.createNewEmployerWPN('Businness13s');
 
-// })
+})
 
-// test("Verify for new employer Contact Details are added successfully @demorun", async ({ employerIdentitiesPage }) => {
-//     //await employerIdentitiesPage.createNewEmployer('098765800', '3333800', 'MS-EMP-909300')
-//     await employerIdentitiesPage.addContactDetails('Businness06');
+test("Verify for new employer Contact Details are added successfully @demorun", async ({ employerIdentitiesPage }) => {
+    //await employerIdentitiesPage.createNewEmployer('098765800', '3333800', 'MS-EMP-909300')
+    await employerIdentitiesPage.addContactDetails('Businness06');
 
-// })
+})
 
-// test("Verify existing employer data can be edited successfully @demorun", async ({ employerIdentitiesPage }) => {
-//    // await employerIdentitiesPage.createNewEmployer('098765901', '3333901', 'MS-EMP-909901');
-//     //test.setTimeout(4000);
-//     await employerIdentitiesPage.updateExistingEployer('Bussiness06');
+test("Verify existing employer data can be edited successfully @demorun", async ({ employerIdentitiesPage }) => {
+   // await employerIdentitiesPage.createNewEmployer('098765901', '3333901', 'MS-EMP-909901');
+    //test.setTimeout(4000);
+    await employerIdentitiesPage.updateExistingEployer('Bussiness06');
 
-// })
+})
