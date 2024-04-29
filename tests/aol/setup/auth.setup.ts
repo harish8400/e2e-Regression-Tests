c0edf306d5c1032a6e8cfb25a5aa51ae8505b22f
minal.tate <minal.tate@grow.inc>
2024-04-29 15:50:41 +0530

    token code change

--- tests/aol/setup/auth.setup.ts
+++ tests/aol/setup/auth.setup.ts
@@ -3,10 +3,10 @@ import { Admins } from '../../../src/aol/data/admins';
 import { ENVIRONMENT_CONFIG } from '../../../config/environment_config';
 import { allure } from "allure-playwright";
 import { fundName } from '../../../src/aol/utils_aol';
-import { TokenManager } from '../../../src/aol_api/token_manager'; // Import TokenManager class
 
 
 
+const authFile = 'playwright/.auth/user.json';
 setup(fundName() + "authenticate", async ({ page }) => {
     await allure.parentSuite("Login"); 
     let authToken: string | null = null; 
@@ -16,11 +16,12 @@ setup(fundName() + "authenticate", async ({ page }) => {
         const authorization = headers['authorization'];
         if (url.startsWith('https://middleware-saturn.dev.tinasuper.com/users/me') && authorization) {
             authToken = authorization;
-            // console.log(`Found authorization token: ${authToken}`);
-            // Store authToken using TokenManager
-            TokenManager.setToken(authToken);
+           process.env.TOKEN = authToken;
         }
-    });    // Perform authentication steps. Replace these actions with your own.
+    });   
+
+    
+    // Perform authentication steps. Replace these actions with your own.
     let admin = Admins.getAdminByUsername("admin@tinasuper.com");
     await page.goto(ENVIRONMENT_CONFIG.aolURL);
     await page.getByRole('button').nth(2).click();
@@ -29,14 +30,11 @@ setup(fundName() + "authenticate", async ({ page }) => {
     await page.keyboard.press('Tab');
     await page.getByRole('button', { name: 'Log in' }).click();
     // Wait until the page receives the cookies.
-    //
     // Sometimes login flow sets cookies in the process of several redirects.
     // Wait for the final URL to ensure that the cookies are actually set.
     //await page.waitForURL('https://github.com/');
     // Alternatively, you can wait until the page reaches a state where all cookies are set.
     await new Promise(resolve => setTimeout(resolve, 4000));
     await expect(page.getByText('Open Cases', { exact: true })).toBeVisible();
-    
-    TokenManager.getToken(authToken!);
-    // console.log(`Token getting from get Method : `+ authToken)
+    await page.context().storageState({ path: authFile });
 });
