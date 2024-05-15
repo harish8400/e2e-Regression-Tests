b801449c500dced42c2ad720def4d20944d1aa96
minal.tate <minal.tate@grow.inc>
2024-05-15 19:09:46 +0530

    code change

--- src/aol/pom/Pension/account_info.ts
+++ src/aol/pom/Pension/account_info.ts
@@ -174,9 +174,9 @@ export class AccountInfoPage extends BasePage {
 
     async ProcessTab() {
         await this.processesLink.click();
-        await this.sleep(5000);
+        await this.sleep(3000);
         await this.memberaccount.click();
-        await this.page.getByRole('cell', { name: 'In Review' }).locator('div').click();
+        await this.review.click();
         await this.page.reload();
         await this.sleep(3000);
         const caseId = this.page.locator("(//div[@class='gs-column case-table-label']/following-sibling::div)[1]");
