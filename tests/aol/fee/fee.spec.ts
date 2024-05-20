48f52a989da616d31e9d392806b20e9c947847c5
anilkumar.shanthalingappa <anilkumar.shanthalingappa@grow.inc>
2024-05-20 16:28:54 +0530

    Fee test update

--- tests/aol/fee/fee.spec.ts
+++ tests/aol/fee/fee.spec.ts
@@ -15,7 +15,7 @@ export const test = base.extend<{ apiRequestContext: APIRequestContext }>({
 });
 
 test.beforeEach(async ({ navBar }) => {
-  test.setTimeout(300000);
+  test.setTimeout(400000);
   await navBar.selectProduct();
   await allure.suite("Fee");
   await allure.parentSuite(process.env.PRODUCT!);
